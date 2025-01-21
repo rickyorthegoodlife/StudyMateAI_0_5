bc84ba6c9a2c85a804ad06c7a85b707d046d8fe2
rickyorthegoodlife <desse.eric@gmail.com>
2025-01-22 00:24:30 +0100

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export const API_KEY = {
-  OPENAI: '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',
-  GEMINI: 'AIzaSyCc7MeKgxn27ykGebBVaZ50zn6mb8769Uk'
+  OPENAI: '',
+  GEMINI: ''
 }
